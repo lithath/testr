01a3bca6f89259b37452f1669dcf1e61f00e3c34
littishathankam <littishathankam@HP>
2018-01-19 12:46:55 +0530

    ini

---
+++ testr/src/testr/com/Testr.java
@@ -0,0 +1,12 @@
+package testr.com;
+
+public class Testr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test 111");
+
+System.out.println("Test222");
+	}
+
+}
